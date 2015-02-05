bf25cc855ecf6c3a1d13d6c05e626fc06dbccef2
kangkyu <kangkyu1111@gmail.com>
2015-02-05 11:59:20 -0800

    user should be able to open the page all event listed

--- test/unit/event-index-controller_test.js
+++ test/unit/event-index-controller_test.js
@@ -4,24 +4,28 @@ describe('eventIndexCtrl', function(){
 
     var mockEvents = [
         {
+            _id: "1",
             title: "Olympics",
             address: "Rio De Janeiro, Brazil",
             date: "Aug 21, 2016",
             time: "1pm"
         },
         {
+            _id: "2",
             title: "Super Bowl",
             address: "Phoenix, Arizona",
             date: "Feb 1, 2015",
             time: "2am"
         },
         {
+            _id: "3",
             title: "World Cup",
             address: "Rio De Janeiro, Brazil",
             date: "Jun 12, 2014",
             time: "2pm"
         },
         {
+            _id: "4",
             title: "World Series",
             address: "San Francisco, California",
             date: "Oct 21, 2014",
--- test/unit/event-service_test.js
+++ test/unit/event-service_test.js
@@ -4,30 +4,42 @@ describe('eventService', function(){
 
     var mockEvents = [
         {
+            _id: "1",
             title: "Olympics",
             address: "Rio De Janeiro, Brazil",
             date: "Aug 21, 2016",
             time: "1pm"
         },
         {
+            _id: "2",
             title: "Super Bowl",
             address: "Phoenix, Arizona",
             date: "Feb 1, 2015",
             time: "2am"
         },
         {
+            _id: "3",
             title: "World Cup",
             address: "Rio De Janeiro, Brazil",
             date: "Jun 12, 2014",
             time: "2pm"
         },
         {
+            _id: "4",
             title: "World Series",
             address: "San Francisco, California",
             date: "Oct 21, 2014",
             time: "3pm"
         }
     ];
+
+    var idPicked = "3", eventPicked = {
+            _id: "3",
+            title: "World Cup",
+            address: "Rio De Janeiro, Brazil",
+            date: "Jun 12, 2014",
+            time: "2pm"
+        };
 
     var eventService;
     beforeEach(function(){
@@ -54,4 +66,17 @@ describe('eventService', function(){
             expect(eventService.getAll()).toEqual(mockEvents);
         });
     });
+
+    describe('getById', function(){
+        it("gets an event by its id when there's a match", function(){
+            eventService.setAll(mockEvents);
+            expect(eventService.getById(idPicked)).toEqual(eventPicked);
+        });
+
+        it("gets empty object when there's no match by id", function(){
+            eventService.setAll(mockEvents);
+            expect(eventService.getById("nomatch")).toEqual({});
+        });
+    });
 });
+
