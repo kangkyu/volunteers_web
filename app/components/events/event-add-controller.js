4e856e8133341411dfeb22f63b639488bf5b8435
kangkyu <kangkyu1111@gmail.com>
2015-02-13 13:31:57 -0800

    fix event add controller and its test

--- app/components/events/event-add-controller.js
+++ app/components/events/event-add-controller.js
@@ -3,11 +3,13 @@ angular
 .controller('eventAddCtrl', ['$scope', 'eventService', '$location',
 function($scope, eventService, $location){
 
-    $scope.event = {};
-
-    $scope.addButton = function(){
-        eventService.addEvent($scope.event).success(function(data){
-            $location.path('/events/' + data._id);
+    $scope.addButton = function(event){
+        eventService.addEvent(event).success(function(data){
+            $scope.event = data;
+            eventService.loadAll().success(function(data){
+                $scope.events = data;
+            });
+            $location.path('/events/' + $scope.event._id);
         });
     };
 }]);
